1d53a78067b73596f2765993bdbd424311500379
gaylesha <gaylesha@student.21-school.ru>
2025-02-21 21:15:46 +0500

    Add tests for s21_sub_matrix

--- src/s21_sub_matrix.c
+++ src/s21_sub_matrix.c
@@ -1,12 +1,23 @@
 #include "s21_matrix.h"
 
 int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-  s21_create_matrix(A->rows, A->columns, result);
+  int check_matrix = OK;
 
-  for (int i = 0; i < A->rows; i++) {
-    for (int j = 0; j < B->columns; j++) {
-      result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+  if (!A || !B || !result || !A->matrix || !B->matrix || !result->matrix){ 
+    check_matrix = INCORRECT_MATRIX;
+  }
+  else if (A->rows != B->rows || A->columns != B->columns ||
+B->rows != result->rows || B->rows != result->columns) {
+    check_matrix = CALCULATION_ERROR;
+  } 
+  else {
+    for (int i = 0; i < A->rows; i++) {
+      for (int j = 0; j < B->columns; j++) {
+        result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+      }
     }
   }
-  return SUCCESS;
+
+    return check_matrix;
 }
+// make function s21_check ipnut
