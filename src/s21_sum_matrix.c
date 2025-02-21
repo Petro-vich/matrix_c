efd06f1899596a11d61b5b9767e715f6ec4ef904
gaylesha <gaylesha@student.21-school.ru>
2025-02-21 15:21:06 +0500

    Add Check_Matrix

--- src/s21_sum_matrix.c
+++ src/s21_sum_matrix.c
@@ -1,8 +1,15 @@
 #include "s21_matrix.h"
 
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-  if (A->rows != B->rows || A->columns != B->columns) {
-  } else {
+  int check_matrix = OK;
+
+  if (!A || !B || !A->matrix || !B->matrix){ 
+    check_matrix = INCORRECT_MATRIX;
+  }
+  else if (A->rows != B->rows || A->columns != B->columns) {
+    check_matrix = CALCULATION_ERROR;
+  } 
+  else {
     for (int i = 0; i < A->rows; i++) {
       for (int j = 0; j < B->columns; j++) {
         result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
