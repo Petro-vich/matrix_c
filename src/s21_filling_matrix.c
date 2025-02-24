dc6b9d669d2f6c558a23f5c99e57a61e4d585f37
gaylesha <petr.vasilyev.1999@gmail.com>
2025-02-24 19:59:38 +0500

    random filling

--- src/s21_filling_matrix.c
+++ src/s21_filling_matrix.c
@@ -4,7 +4,7 @@ void s21_filling_matrix(matrix_t *new_matrix, double number) {
   for (int i = 0; i < new_matrix->rows; i++) {
     for (int j = 0; j < new_matrix->columns; j++) {
       new_matrix->matrix[i][j] = number;
-      number += 5.0;
+      number = rand() % 100;
     }
   }
 }
