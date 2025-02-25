a2eed02996ee81778f2b47a5f968e6a75919c45c
gaylesha <petr.vasilyev.1999@gmail.com>
2025-02-25 23:07:28 +0500

    clang

--- src/s21_mult_number.c
+++ src/s21_mult_number.c
@@ -2,7 +2,7 @@
 
 int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
   int status_matrix = s21_check_matrices(2, A, result);
-  if (status_matrix == OK){
+  if (status_matrix == OK) {
     for (int i = 0; i < A->rows; i++) {
       for (int j = 0; j < A->columns; j++) {
         result->matrix[i][j] = A->matrix[i][j] * number;
