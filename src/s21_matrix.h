3230de2a633a153baa92e37a52b06f792ed4eef9
gaylesha <petr.vasilyev.1999@gmail.com>
2025-02-25 23:08:30 +0500

    add #include math.h

--- src/s21_matrix.h
+++ src/s21_matrix.h
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <math.h>
 
 enum ERROR_CODES { OK, INCORRECT_MATRIX, CALCULATION_ERROR };
 
@@ -16,7 +17,7 @@ typedef struct matrix_struct {
   int columns;
 } matrix_t;
 
-void s21_print_matrix(matrix_t *A);
+void s21_print_matrix(matrix_t *  A);
 int s21_create_matrix(int rows, int columns, matrix_t *result);
 void s21_remove_matrix(matrix_t *A);
 int s21_eq_matrix(matrix_t *A, matrix_t *B);
@@ -25,10 +26,11 @@ int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 int s21_mult_number(matrix_t *A, double number, matrix_t *result);
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 int s21_transpose(matrix_t *A, matrix_t *result);
-int s21_calc_complements(matrix_t *A, matrix_t *result);
+int s21_determinant(matrix_t *A, double *result);
 // aux_fnc
 void s21_copy_matrix(matrix_t *A, matrix_t *result);
 void s21_filling_matrix(matrix_t *new_matrix, double number);
 int s21_memcmp(const void *str1, const void *str2, int n);
 int s21_check_matrices(int count, ...);
+
 #endif
