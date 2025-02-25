889aee4dcb630aa3da63915dbda5f38bb436f638
gaylesha <petr.vasilyev.1999@gmail.com>
2025-02-25 23:06:55 +0500

    clang

--- src/s21_transpose.c
+++ src/s21_transpose.c
@@ -2,9 +2,9 @@
 
 int s21_transpose(matrix_t *A, matrix_t *result) {
   int check_matrix = s21_check_matrices(1, A);
-  
+
   if (check_matrix == OK) {
-  s21_create_matrix(A->columns, A->rows, result);
+    s21_create_matrix(A->columns, A->rows, result);
     for (int i = 0; i < result->rows; i++) {
       for (int j = 0; j < result->columns; j++) {
         result->matrix[i][j] = A->matrix[j][i];
