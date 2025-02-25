4b4b8625f302f072269b1d9eee937baeea114bb2
gaylesha <petr.vasilyev.1999@gmail.com>
2025-02-25 23:08:53 +0500

    clang

--- src/s21_eq_matrix.c
+++ src/s21_eq_matrix.c
@@ -2,7 +2,7 @@
 
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
   int check_matrix = s21_check_matrices(2, A, B) ? FAILURE : SUCCESS;
-  
+
   if (check_matrix) {
     for (int i = 0; i < A->rows; i++) {
       for (int j = 0; j < A->columns; j++) {
