f388a6a28426c85273d628326aaec81c906ab3d3
gaylesha <petr.vasilyev.1999@gmail.com>
2025-03-10 22:00:25 +0500

    add int s21_calc_complements(matrix_t *A, matrix_t *result);

--- src/s21_matrix.h
+++ src/s21_matrix.h
@@ -32,5 +32,6 @@ void s21_copy_matrix(matrix_t *A, matrix_t *result);
 void s21_filling_matrix(matrix_t *new_matrix, double number);
 int s21_memcmp(const void *str1, const void *str2, int n);
 int s21_check_matrices(int count, ...);
+int s21_calc_complements(matrix_t *A, matrix_t *result);
 
 #endif
